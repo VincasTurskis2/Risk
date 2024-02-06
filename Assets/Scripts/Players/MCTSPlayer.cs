916152f9ea56240be40ff246780d6324996c7e52
Vincas Turskis Desktop <vincas.turskis@gmail.com>
2024-02-06 22:59:54 +0100

    Worked on MCTS and dissertation

--- Assets/Scripts/Players/MCTSPlayer.cs
+++ Assets/Scripts/Players/MCTSPlayer.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using UnityEditorInternal;
+using System;
 
 // An implementation of a player using Monte Carlo tree search.
 public class MCTSPlayer : Player
 {
     public float timeForSearch = 1f;
+    public double C = Math.Sqrt(2);
     public MCTSPlayer(GameState state, PlayerData data, bool is2PlayerGame) : base(state, data, is2PlayerGame)
     {
     }
@@ -35,7 +37,7 @@ public class MCTSPlayer : Player
     public List<PlayerAction> PerformMTCS()
     {
         List<PlayerAction> result = new();
-        GameStateTreeNode root = new GameStateTreeNode(_gameState);
+        GameStateTreeNode root = new GameStateTreeNode(_gameState, null);
         float curTime = 0f;
         while (curTime < timeForSearch)
         {
@@ -51,8 +53,50 @@ public class MCTSPlayer : Player
         return result;
     }
 
-    public IGameStatePlayerView Select(GameStateTreeNode tree)
+    public GameStateTreeNode Select(GameStateTreeNode curNode)
     {
-        return tree.state;
+        if(curNode.numberOfPlaythoughs == 0) return curNode;
+
+        return curNode;
+    }
+
+
+
+    public float heuristicEvaluation(GameState state, Player player)
+    {
+        return troopSharePercent(state, player);
+    }
+
+    public float troopSharePercent(GameState state, Player player)
+    {
+        int totalOwnTroops = 0, totalOtherTroops = 0;
+        for(int i = 0; i < state.map.Territories.Length; i++)
+        {
+            if(state.map.Territories[i].Owner.Equals(player.GetData().playerName))
+            {
+                totalOwnTroops += state.map.Territories[i].TroopCount;
+            }
+            else
+            {
+                totalOtherTroops += state.map.Territories[i].TroopCount;
+            }
+        };
+        if(totalOwnTroops + totalOtherTroops == 0) return 0;
+        
+        return totalOwnTroops/(totalOwnTroops + totalOtherTroops);
+        
+    }
+    public float UCB1(GameStateTreeNode node)
+    {
+        double result = 0;
+        if(node.numberOfPlaythoughs != 0)
+        {
+            result = node.cumulativeHeuristicValue / node.numberOfPlaythoughs;
+        }
+        if(node.parent != null)
+        {
+            result += C * Math.Sqrt(Math.Log(node.parent.numberOfPlaythoughs)/node.numberOfPlaythoughs);
+        }
+        return (float)result;
     }
 }
--- Assets/Scripts/Tree/GameStateTreeNode.cs
+++ Assets/Scripts/Tree/GameStateTreeNode.cs
@@ -3,28 +3,31 @@ using System.Collections.Generic;
 public class GameStateTreeNode
 {
     public GameState state {get; private set;}
+    public GameStateTreeNode parent {get; private set;}
     public List<GameStateTreeNode> children {get; private set;}
 
-    public float heuristicValue;
+    public float cumulativeHeuristicValue;
     public int numberOfPlaythoughs;
 
-    public GameStateTreeNode(GameState Value)
+    public GameStateTreeNode(GameState Value, GameStateTreeNode Parent)
     {
         state = Value;
         children = new List<GameStateTreeNode>();
         numberOfPlaythoughs = 0;
-        heuristicValue = 0;
+        cumulativeHeuristicValue = 0;
+        parent = Parent;
     }
-    public GameStateTreeNode(IGameStatePlayerView Value)
+    public GameStateTreeNode(IGameStatePlayerView Value, GameStateTreeNode Parent)
     {
         state = (GameState) Value;
         children = new List<GameStateTreeNode>();
         numberOfPlaythoughs = 0;
-        heuristicValue = 0;
+        cumulativeHeuristicValue = 0;
+        parent = Parent;
     }
 
     public void AddChild(GameState child)
     {
-        children.Add(new GameStateTreeNode(child));
+        children.Add(new GameStateTreeNode(child, this));
     }
 }
