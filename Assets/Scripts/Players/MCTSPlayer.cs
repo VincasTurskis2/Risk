d16ca69157c9368dd7d69cc61f73cd2b8fcdb79b
Vincas Turskis Desktop <v.turskis@lancaster.ac.uk>
2024-03-04 00:29:53 +0100

    Written a lot of the design section of the report

--- Assets/Scripts/Players/MCTSPlayer.cs
+++ Assets/Scripts/Players/MCTSPlayer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using System;
-using UnityEditorInternal;
 
 // An implementation of a player using Monte Carlo tree search.
 public class MCTSPlayer : Player
